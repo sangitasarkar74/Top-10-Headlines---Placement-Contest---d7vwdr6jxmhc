ff69949b4d297570500786815770d5d4a6c36863
sangitasarkar74 <sangitasarkar@gmail.com>
2023-04-02 16:23:39 +0530

    updated again

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,27 @@ const App = () => {
 
   useEffect(() => {
     setLoading(true);
-
     fetch(
-      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=18627a52ea958ef443c09c08adb9a4a5&max=10&lang=en`
+      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=9fd8b22cbe7061cc139ea019baad35a5
+
+      &max=10&lang=en`
     )
       .then((res) => res.json())
       .then((data) => setNewsData(data.articles));
     setLoading(false);
   }, [category]);
+
   console.log("newsDta", newsData);
 
   const handleChange = (e) => {
     setCategory(e.target.value);
+    setLoading(true);
   };
 
   return (
     <div id="main">
       <h1 className="heading">Top 10 {category} news.</h1>
+
       <select value={category} onChange={handleChange}>
         <option value="general">General</option>
         <option value="business">Business</option>
